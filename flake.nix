a77cbea0a04dbf7a99be95d527def2a90aa3f894
Samuel Leathers <samuel.leathers@iohk.io>
2024-02-27 11:01:57 -0500

    cardano-parts: update to 8.8.1 node

--- flake.nix
+++ flake.nix
@@ -1,13 +1,11 @@
 {
   description = "Cardano New Parts Project";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    cardano-parts.url = "github:input-output-hk/cardano-parts/sl/8.6.0-scripts";
-    cardano-node-ng.url = "github:input-output-hk/cardano-node/td/8.6";
-    cardano-cli-ng.url = "github:input-output-hk/cardano-cli/cardano-cli-8.13.0.0";
+  inputs = rec {
+    nixpkgs.follows = "cardano-parts/nixpkgs";
+    nixpkgs-unstable.follows = "cardano-parts/nixpkgs-unstable";
+    flake-parts.follows = "cardano-parts/flake-parts";
+    cardano-parts.url = "github:input-output-hk/cardano-parts/next-2024-02-20";
   };
 
   outputs = inputs: let
@@ -24,6 +22,7 @@
           inputs.cardano-parts.flakeModules.shell
           inputs.cardano-parts.flakeModules.entrypoints
           inputs.cardano-parts.flakeModules.jobs
+          inputs.cardano-parts.flakeModules.lib
         ];
       systems = ["x86_64-linux" "aarch64-darwin"];
       debug = true;
